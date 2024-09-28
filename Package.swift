0cc497c52af9e0c98873d05ce2da5765ffb0973f
Ilya Gulya <ilya@gulya.me>
2024-09-28 09:38:59 +0500

    Fix

--- Package.swift
+++ Package.swift
@@ -7,12 +7,12 @@ let package = Package(
     .library(name: "Stencil", targets: ["Stencil"])
   ],
   dependencies: [
-    .package(url: "https://github.com/IlyaGulya/PathKit-musl", branch: "master"),
+    .package(url: "https://github.com/IlyaGulya/PathKit", branch: "master"),
     .package(url: "https://github.com/kylef/Spectre.git", from: "0.10.1")
   ],
   targets: [
     .target(name: "Stencil", dependencies: [
-      .product(name: "PathKit", package: "PathKit-musl"),
+      "PathKit"
     ]),
     .testTarget(name: "StencilTests", dependencies: [
       "Stencil",
